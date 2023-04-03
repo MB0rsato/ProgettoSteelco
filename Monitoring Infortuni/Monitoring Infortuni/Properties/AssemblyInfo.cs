f90e6874b5509fc63d59d8c0fc20b5b9640665b5
MB0rsato <118889102+MB0rsato@users.noreply.github.com>
2023-04-03 16:43:38 +0200

    Creazione Progetto

---
+++ Monitoring Infortuni/Monitoring Infortuni/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("Monitoring_Infortuni")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Monitoring_Infortuni")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta il valore di ComVisible su falso, i tipi nell'assembly non sono più visibili
+// dai componenti COM. Se è necessario accedere al tipo nell'assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato per creare l'ID di typelib
+[assembly: Guid("f692aa86-67ed-4bf6-8a5c-4421452c49c5")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai quattro valori seguenti:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero build
+//      Revisione
+//
+// È possibile specificare tutti i valori o lasciare i valori predefiniti per Revisione e Numeri build
+// utilizzando l'asterisco (*) come illustrato di seguito:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
