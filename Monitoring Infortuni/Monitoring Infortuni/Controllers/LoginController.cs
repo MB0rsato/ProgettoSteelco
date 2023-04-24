78b7316a99268b3f57f3e0382ff5a368c6ba7162
MB0rsato <118889102+MB0rsato@users.noreply.github.com>
2023-04-24 18:04:16 +0200

    Login Update

--- Monitoring Infortuni/Monitoring Infortuni/Controllers/LoginController.cs
+++ Monitoring Infortuni/Monitoring Infortuni/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Monitoring_Infortuni.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,18 @@ namespace Monitoring_Infortuni.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult ProcessLogin(AdminModel adminModel)
+        {
+            if(adminModel.Password == "ciao" && adminModel.UserName == "admin")
+            {
+                return View("LoginSuccess");
+            }
+            else
+            {
+                return View("LoginFailed");
+            }
+            
         }
     }
 }
